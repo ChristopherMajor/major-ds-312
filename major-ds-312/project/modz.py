d1ceaecd1d23c44fcb2e4903317914b4287ba9f0
ChristopherMajor <majorglasslabs@gmail.com>
2020-04-07 16:22:09 -0700

    Add functionality

--- major-ds-312/project/modz.py
+++ major-ds-312/project/modz.py
@@ -1,7 +1,12 @@
 def enlarge(n):
     return n * 100
 
-x = int(input("Input number:"))
-result = enlarge(x)
-print(result)
+# x = int(input("Input number:"))
+# result = enlarge(x)
+# print(result)
 
+if __name__ =="__main__":
+    
+    x = int(input("Input number:"))
+    result = enlarge(x)
+    print(result)
--- major-ds-312/project/my_script.py
+++ major-ds-312/project/my_script.py
@@ -1,6 +1,9 @@
 import pandas
+from project.modz import enlarge
 
 print("uWu")
 
 df = pandas.DataFrame({'state': ['CT', "CO", "CA","TX"]})
 print(df.head())
+
+print(enlarge(9))
