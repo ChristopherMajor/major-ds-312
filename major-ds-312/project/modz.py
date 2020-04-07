873a626ea7882a9eea8ca0cd761304014aa98bd4
ChristopherMajor <majorglasslabs@gmail.com>
2020-04-07 15:03:12 -0700

    extra code

---
+++ major-ds-312/project/modz.py
@@ -0,0 +1,7 @@
+def enlarge(n):
+    return n * 100
+
+x = int(input("Input number:"))
+result = enlarge(x)
+print(result)
+
--- major-ds-312/project/my_script.py
+++ major-ds-312/project/my_script.py
@@ -1,3 +1,6 @@
 import pandas
 
 print("uWu")
+
+df = pandas.DataFrame({'state': ['CT', "CO", "CA","TX"]})
+print(df.head())
